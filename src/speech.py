1447bba30f7f723eadeb26fb3f265edff9e80434
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-05-23 22:05:12 -0300

    Create speech.py

---
+++ src/speech.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+import pyttsx3
+
+r = sr.Recognizer()
+
+engine = pyttsx3.init()
+
+with sr.Microphone() as mic:
+    r.adjust_for_ambient_noise(mic)
+    print("AAAÃÃÃNNHHÓÓOOOØOOONNN")
+    engine.say("AAAÃÃÃNNHHÓÓOOOØOOONNN")
+    engine.runAndWait()
+
+    audio = r.listen(mic)
+
+    try:
+        texto = r.recognize_google(audio, language='pt-BR')
+        print(texto)
+    except sr.UnknownValueError:
+        print("AAAÃÃÃNNHHÓÓOOOØOOONNN")
+    except sr.RequestError as e:
+        print(f"AAAÃÃÃNNHHÓÓOOOØOOONNN: {e}")
+
