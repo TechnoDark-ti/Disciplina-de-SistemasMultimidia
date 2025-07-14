721f7d56b590a4450c1e7d4d275973c53a64ed9f
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-07-14 11:02:11 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,32 +13,29 @@ echo "Iniciando a configuração do ambiente Yolo Lixo Costeiro... Aguarde!"
 
 OS_TYPE="$(uname-s)"
 
-if ["$OS_TYPE" == "Darwin"]';then
-   echo"Sistema Detectado: macOS"
+if ["$OS_TYPE" == "Darwin"]; then
+   echo "Sistema Detectado: macOS"
    brew install python@3.11
-elif ["OS_TYPE" == ""Linux"]; then
+elif ["OS_TYPE" == "Linux"]; then
    sudo apt update
    sudo apt install python3.11 python3.11-virtualenv -y
 else
-   echo"Sistema não suportado! $OS_TYPE"
-   exite 1
+   echo "Sistema não suportado! $OS_TYPE"
+   exit 1
 fi
 
 python3.11 -m venv Disciplina-de-SistemasMultimidia
 source Disciplina-de-SistemasMultimidia/bin/activate
 
-
 pip3 install --upgrade pip
 pip3 install -r requeriments.txt
 pip3 install labelme
-
 
 mkdir -p data/beachlitter/images/train
 mkdir -p data/beachlitter/images/val
 mkdir -p data/beachlitter/labels/train
 mkdir -p data/beachlitter/labels/val
 mkdir -p runs/
-
 
 cat <<EOF > data/beachlitter.yaml
 path: data/beachlitter
@@ -47,7 +44,6 @@ val: images/val
 nc: 1
 names: ["lixo artificial"]
 EOF
-
 
 echo "Setup finalizado com sucesso!"
 echo "Ative o ambiente com: source bin/activate"
