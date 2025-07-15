166e669e4a2bbd2d198c9d7bb532d7164a04bc2e
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-07-15 20:07:58 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,24 +11,24 @@ EOF
 
 echo "Iniciando a configuração do ambiente Yolo Lixo Costeiro... Aguarde!"
 
-OS_TYPE="$(uname-s)"
+OS_TYPE="$(uname -s)"
 
-if ["$OS_TYPE" == "Darwin"]; then
-   echo "Sistema Detectado: macOS"
-   brew install python@3.11
-elif ["OS_TYPE" == "Linux"]; then
-   sudo apt update
-   sudo apt install python3.11 python3.11-virtualenv -y
-else
-   echo "Sistema não suportado! $OS_TYPE"
-   exit 1
-fi
+#if ["$OS_TYPE" == "Darwin"]; then
+#   echo "Sistema Detectado: macOS"
+#   brew install python@3.11
+#elif ["OS_TYPE" == "Linux"]; then
+#   sudo apt update
+#   sudo apt install python3.11 python3.11-virtualenv -y
+#else
+#   echo "Sistema não suportado! $OS_TYPE"
+#   exit 1
+#fi
 
-python3.11 -m venv Disciplina-de-SistemasMultimidia
-source Disciplina-de-SistemasMultimidia/bin/activate
+#python3.11 -m venv Disciplina-de-SistemasMultimidia
+#source Disciplina-de-SistemasMultimidia/bin/activate
 
-pip3 install --upgrade pip
-pip3 install -r requeriments.txt
+#pip3 install --upgrade pip
+#pip3 install -r requeriments.txt
 pip3 install labelme
 
 mkdir -p data/beachlitter/images/train
